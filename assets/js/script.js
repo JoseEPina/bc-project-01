b4a752aa157e986259df7557ef084ba5b420bd01
JoseEPina <jose.edpina@gmail.com>
2021-09-01 23:46:03 -0600

    Created dropdown menu w/ Spotify's Genre List

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,53 @@
 var genreList = [];
 
-fetch("https://api.spotify.com/v1/recommendations/available-genre-seeds", {
-   headers: {
-      Accept: "application/json",
-      Authorization:
-         "Bearer BQDPL4v9lWf2xZDnwvo5vtsuByC2yN1_OHVytpZhuHmyHZzSKl6XQQZIkp48UX4mPLqr4O4swYq1RmBn1evW7iE4PbuOPuN0VTkvnSvfzpxNCX7A-8OU7UstXXY0SPe35GawjbDZi_8",
-      "Content-Type": "application/json",
-   },
-})
-   .then((response) => response.json())
-   .then((data) => {
-      genreList = data.genres;
-      localStorage.setItem("myGenreList", JSON.stringify(genreList));
-   });
+function getGenreList() {
+   fetch("https://api.spotify.com/v1/recommendations/available-genre-seeds", {
+      headers: {
+         Accept: "application/json",
+         Authorization:
+            "Bearer BQBIHx9qhjBKUbsE3yMuKUWbEjZE1OhU7aB0N-xtENwNw5sosmKws9iJkyO8JtRjp-Exv0uA2avhlt7ho7-IofG4i-KSNR_lTH3Dnw2SG-20mBJcgZHuvXq9HHHjK_-yy9MzvN63-fc",
+         "Content-Type": "application/json",
+      },
+   })
+      .then((response) => response.json())
+      .then((data) => {
+         genreList = data.genres;
+         localStorage.setItem("myGenreList", JSON.stringify(genreList));
+         console.log("ls out", genreList);
+      });
+   genreList = JSON.parse(localStorage.getItem("myGenreList"));
+}
 
+function createGenreDOM() {
+   var divElement = document.createElement("div");
+   divElement.className = "container";
+
+   var labelElement = document.createElement("label");
+   labelElement.for = "genre-list-group";
+   labelElement.textContent = "Choose a music genre : ";
+
+   var selectElement = document.createElement("select");
+   selectElement.className = "genre-names";
+   selectElement.id = "genre-names";
+
+   var tempLength = genreList.length;
+
+   for (var i = 0; i < tempLength; i++) {
+      var optionElement = document.createElement("option");
+      optionElement.textContent = genreList[i];
+      optionElement.className = "genre-list-item";
+      selectElement.appendChild(optionElement);
+      console.log(optionElement);
+   }
+
+   document.getElementById("spotify-header").after(divElement);
+   divElement.appendChild(labelElement);
+   divElement.appendChild(selectElement);
+}
+
+function generatePlaylist() {
+   getGenreList();
+   createGenreDOM();
+}
+
+generatePlaylist();
