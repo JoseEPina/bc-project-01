499c1efdc505f1633020e89e6b8bac20e49ce0ce
Andrew Durham <andrewdurham1094@gmail.com>
2021-09-05 18:58:11 -0600

    finally settled on a Lyrics API

--- assets/js/lyrics.js
+++ assets/js/lyrics.js
@@ -1,38 +1,24 @@
-var apiKey = '7e0363dc461206b83c2b3b6300cd4a3c'
+var apiKey = 'ebd5f559316479fee95ab51cbb0ffad5'
 
-function lyricSearch(song) {
-
-    var trackId = getTrackID(song)
+function getTrackID(songArtist, songTitle) {
     //format the api url
-    var apiUrl = 'https://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=' + trackId + '&apikey=' + apiKey
-    fetch(apiUrl, { mode: 'no-cors'}).then(function(response) {
-        //request was successful
-        if(response.ok) {
-            response.json().then(function(data) {
-                console.log(data)
-            });
-        } else {
-            alert("Error: Lyrics Not Found");
-        }
-    }).catch(function(error) {
-        alert("Unable to connect to MusixMatch")
-    })
-}
-
-function getTrackID(songTitle) {
-    //format the api url
-    var apiUrl = 'https://api.musixmatch.com/ws/1.1/track.search?q_track=' + songTitle + '&apikey=' + apiKey
-    fetch(apiUrl, { mode: 'no-cors'}).then(function(response) {
+    var apiUrl = 'https://api.vagalume.com.br/search.php?art=' + songArtist + '&mus=' + songTitle + '&apikey=' + apiKey
+    fetch(apiUrl).then(function(response) {
         if (response.ok) {
             response.json().then(function(data) {
-                var commontrack_id = data.body.track_list[0].commontrack_id
-                return commontrack_id
+                var lyricsEl = document.getElementById('test')
+                var lyrics = document.createElement('p')
+                lyrics.textContent = data.mus[0].text
+                lyricsEl.appendChild(lyrics)
+                console.log(data)
             })
         } else {
-            alert("Error: Cannot Find Track")
+            alert("Error: Cannot Find Lyrics")
             console.log(apiUrl)
         }
     }).catch(function(error) {
-        alert("Unable to connect to the Track")
+        alert("Unable to connect to the Data")
     })
 }
+
+getTrackID('Quinn xcii', 'Straightjacket')
