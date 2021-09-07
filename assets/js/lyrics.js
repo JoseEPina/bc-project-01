543cef71b0c33b4b60577d50b50f1fcbfc52f19e
Andrew Durham <andrewdurham1094@gmail.com>
2021-09-07 17:10:39 -0600

    added for loop to go through songs in playlist

--- assets/js/lyrics.js
+++ assets/js/lyrics.js
@@ -1,6 +1,6 @@
 var apiKey = 'ebd5f559316479fee95ab51cbb0ffad5'
 
-function getTrackID(songArtist, songTitle) {
+function getTrackLyrics(songArtist, songTitle) {
     //format the api url
     var apiUrl = 'https://api.vagalume.com.br/search.php?art=' + songArtist + '&mus=' + songTitle + '&apikey=' + apiKey
     fetch(apiUrl).then(function(response) {
@@ -9,7 +9,8 @@ function getTrackID(songArtist, songTitle) {
                 var lyricsEl = document.getElementById('test')
                 var lyrics = document.createElement('p')
                 lyrics.textContent = data.mus[0].text
-                lyricsEl.appendChild(lyrics)
+                //lyricsEl.appendChild(lyrics)
+                console.log(lyrics)
                 console.log(data)
             })
         } else {
@@ -21,4 +22,10 @@ function getTrackID(songArtist, songTitle) {
     })
 }
 
-getTrackID('Quinn xcii', 'Straightjacket')
+getTrackLyrics('Queen', 'Killer Queen')
+
+var lyrics = document.getElementsByClassName('playlist-song');
+
+for(i = 0; i < lyrics.length; i++) {
+    lyrics[i].addEventListener('click', getTrackLyrics('Quinn xcii', 'Straightjacket'))
+}
